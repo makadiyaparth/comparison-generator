00a2f937eb61c1bda645a20ff290186b2af7458d
Parth Makadiya <makadiyaparth98@gmail.com>
2022-07-08 16:36:17 +0530

    Initial commit

---
+++ src/main/java/com/pm/comparisongenerator/ComparisonGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.pm.comparisongenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparisonGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComparisonGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pm/comparisongenerator/ComparisonGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.comparisongenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComparisonGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
